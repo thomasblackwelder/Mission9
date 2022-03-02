167c6b9a7ab3d57f5448152b5593b519cd584b6b
thomasblackwelder <83183821+thomasblackwelder@users.noreply.github.com>
2022-03-02 10:53:27 -0700

    2. add ability to have a cart for each session. inherit SessionCart from Cart

---
+++ Mission7/Models/SessionCart.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Mission7.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Mission7.Models
+{
+    public class SessionCart : Cart
+    {
+        // what does an ISession mean? 
+        [JsonIgnore] // prevents and object from being serialized
+        public ISession Session { get; set; }
+
+        public override void AddItem(Book B, int qty)
+        {
+            base.AddItem(B, qty);
+            Session.SetJson("Cart", this);
+        }
+
+        public override void RemoveItem(Book B)
+        {
+            base.RemoveItem(B);
+            Session.SetJson("Cart", this);
+        }
+
+        public override void ClearCart()
+        {
+            base.ClearCart();
+            Session.Remove("Basket");
+        }
+    }
+}
