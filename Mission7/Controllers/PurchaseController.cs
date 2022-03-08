8ac0937a0fd83806b17ba97528a1d4039951f579
thomasblackwelder <83183821+thomasblackwelder@users.noreply.github.com>
2022-03-08 09:40:50 -0700

    6.4 create another controller cause the one we have got too big. Connect the checkout button to the asp-action on the addtocart page Remember you must have a folder in your views with the name of the controller - the controller looks for those pages in the folder of its title

---
+++ Mission7/Controllers/PurchaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission7.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission7.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        public IActionResult Checkout() //checkout is the view name. 
+        {
+            return View(new Purchase());
+        }
+    }
+}
